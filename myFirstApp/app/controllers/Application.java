8ab7476ba41f9a8e7d95ea6b2baa979dc19a6dc2
Friedrich Clausen <friedrich.clausen@blackboard.com>
2013-10-02 14:17:02 +0200

    Got routing example working

--- myFirstApp/app/controllers/Application.java
+++ myFirstApp/app/controllers/Application.java
@@ -7,14 +7,12 @@ import views.html.*;
 
 public class Application extends Controller {
 
-	public static Result hello(String name) {
-		return ok("Hello from root " + name);
+	public static Result index() {
+		return redirect(controllers.routes.Application.hello("Default Dave"));
 	}
-    public static Result index(String name) {
-    	return ok("Hello world! Your name is " + name);
-    	// return notFound("No page here bitch. Yo.");
-    	//return internalServerError("The server made a boo boo");
-    	// return redirect("/tmp");
-    }
+	
+	public static Result hello(String name) {
+		return ok("Hello there " + name);
+	}
 
 }
