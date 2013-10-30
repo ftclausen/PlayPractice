8916808a9ba599180084485811bfc14fce3d7c3c
Friedrich Clausen <friedrich.clausen@blackboard.com>
2013-10-30 17:56:00 +0100

    Get tasks tests working

---
+++ zentasks/app/models/Task.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import play.db.ebean.*;
+
+@Entity
+public class Task extends Model {
+    @Id
+    public Long id;
+    public String title;
+    public boolean done = false;
+    public Date dueDate;
+    @ManyToOne
+    public User assignedTo; // One user; a user may have many tasks
+    public String folder;
+    @ManyToOne
+    public Project project; // One project; projects may have many tasks
+    
+    public static Model.Finder<Long, Task> find = new Model.Finder(Long.class, Task.class);
+
+    public static List<Task> findTodoInvolving(String user) {
+        return find.fetch("project").where()
+                .eq("done", false)
+                .eq("project.members.email", user)
+                .findList();
+    }
+
+    public static Task create(Task task, Long project, String folder) {
+        task.project = Project.find.ref(project);
+        task.folder = folder;
+        task.save();
+        return task;
+    }
+}
--- zentasks/test/models/ModelsTest.java
+++ zentasks/test/models/ModelsTest.java
@@ -11,6 +11,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import play.test.WithApplication;
 import static play.test.Helpers.*;
+import java.util.*;
 
 public class ModelsTest extends WithApplication {
     @Before
@@ -33,6 +34,45 @@ public class ModelsTest extends WithApplication {
         assertNull(User.authenticate("invalid@gmail.com", "bla"));
         assertNull(User.authenticate("another_invalid_user@bla.com", "bla2"));
     }
-}
 
+    @Test
+    public void findProjectsInvolving() {
+        new User("fred@example.com", "Fred", "this is a secret").save();
+        new User("jata@example.com", "Jata", "jata's secret").save();
+
+        Project.create("Play 2", "play", "fred@example.com");
+        Project.create("Play 1", "play", "jata@example.com");
+
+        List<Project> results = Project.findInvolving("fred@example.com");
+        assertEquals(1, results.size());
+        assertEquals("Play 2", results.get(0).name);
+    }
+
+    @Test
+    public void findTodoTasksInvolving() {
+        User fred = new User("fred@example.com", "Fred", "this is a secret");
+        User jata = new User("jata@example.com", "Jata", "jata's secret");
+        fred.save();
+        jata.save();
+
+        Project project = Project.create("Play 2", "play", "fred@example.com");
+        Task t1 = new Task();
+        t1.title = "Do tutorial";
+        t1.project = project;
+        t1.assignedTo = fred;
+        t1.done = true;
+        t1.save();
+
+        Task t2 = new Task();
+        t2.title = "Do next tutorial";
+        t2.project = project;
+        t2.assignedTo = jata;
+        t2.done = true;
+        t2.save();
+
+        List<Task> results = Task.findTodoInvolving("fred@example.com");
+        assertEquals(1, results.size());
+        assertEquals("Do tutorial", results.get(0).title);
+    }
+}
 
