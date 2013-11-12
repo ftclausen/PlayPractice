5a8d8c2be6157d453466b1ace598a9c93b754238
Friedrich Clausen <friedrich.clausen@blackboard.com>
2013-11-12 09:28:22 +0100

    Initial, static, unstyled view screen.

--- zentasks/app/controllers/Application.java
+++ zentasks/app/controllers/Application.java
@@ -1,14 +1,20 @@
 package controllers;
 
+import models.Project;
+import models.Task;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+import static play.data.Form.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("ZenTasks will be here."));
+        // return ok(index.render("ZenTasks will be here."));
+    	return ok(index.render(
+    				Project.find.all(),
+    				Task.find.all()
+    			));
     }
 
 }
